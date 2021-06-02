556b267621b1f612e3b04d82a1be2a8e6d1cb68c
yudican <yudhichandra02@gmail.com>
2021-06-02 19:14:14 +0700

    updated

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+Route::group(['middleware' => ['admin', 'auth:sanctum', 'verified']], function () {
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
+});
