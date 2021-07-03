4e206ce6e495ff813e63c56c2655455e6f8e3fbf
sandi pratama <sandicoding@gmail.com>
2021-07-04 01:01:31 +0700

    add midle HRD new 2

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -17,7 +17,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-         if(Auth::user() && Auth::user()->roles == 'ADMIN' || Auth::user()->roles == 'HRD')
+         if(Auth::user() && Auth::user()->roles == 'ADMIN' && Auth::user()->roles == 'HRD')
         {
             return $next($request);
         }
